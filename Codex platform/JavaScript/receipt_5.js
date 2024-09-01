2452ad654dfe334d4cbdd211e793ccb127e3ef22
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-01 22:18:35 +0545

    Create receipt_5.js

---
+++ Codex platform/JavaScript/receipt_5.js
@@ -0,0 +1,15 @@
+// Write code below 💖
+console.log("==================================");
+console.log("        McDONALD'S RECEIPT");
+console.log("----------------------------------");
+console.log("          Order Details ");
+console.log("----------------------------------");
+console.log("Item          Quantity   Price");
+console.log("----------------------------------");
+console.log("🍔 Burger       1          2.99   ");
+console.log("🍟 Fries        1          2.49   ");
+console.log("🥤 Soda         1          1.99   ");
+console.log("----------------------------------");
+console.log("Total                      7.47   ");
+
+console.log("==================================");
