aa986efff9efc4a33f461b3c2a512e038704ef5a
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-01 18:22:47 +0545

    Create let_and_constant.js

---
+++ Codex platform/let_and_constant.js
@@ -0,0 +1,14 @@
+// Write code below 💖
+
+const email = "xyz@tech.com";
+const userName = "Leeroy Jenkins";
+let progress = 0.75;
+let codingScore = 60;
+let address="brahmandnagar, sivlokgau-29, Anantpuri"
+let verified = true;
+codingScore=90;
+console.log(email);
+console.log(address);
+console.log(`The coding score has been manupulated and now after politics it become ${codingScore}`);
+// To run javascript files:
+//just install node and type node let_and_const.js to run code in your terminal 
