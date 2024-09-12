8bcaa2ce57ab4827760e8b76a2e3bc235c311d43
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-12 06:44:47 +0545

    Update index.js

--- Codex platform/JavaScript/15.Magic 8 ball/index.js
+++ Codex platform/JavaScript/15.Magic 8 ball/index.js
@@ -5,17 +5,17 @@ let answer=["Yes - definitely.","It is decidedly so.","Without a doubt.","Reply 
 
 let question=Math.floor(Math.random()*10);
 if (question==1){
-  console.log(`Question:${question} /n Magic 8 Ball:${answer[0]}`);
+  console.log(`Question:${question} \n Magic 8 Ball:${answer[0]}`);
 } else if(question==2){
-  console.log(`Question:${question} /n Magic 8 Ball:${answer[1]}`);
+  console.log(`Question:${question} \n Magic 8 Ball:${answer[1]}`);
 } else if(question==3){
-  console.log(`Question:${question} /n Magic 8 Ball:${answer[2]}`);
+  console.log(`Question:${question} \n Magic 8 Ball:${answer[2]}`);
 }else if(question==4){
-  console.log(`Question:${question} /n Magic 8 Ball:${answer[3]}`);
+  console.log(`Question:${question} \n Magic 8 Ball:${answer[3]}`);
 } else if(question==5){
-  console.log(`Question:${question} /n Magic 8 Ball:${answer[4]}`);
+  console.log(`Question:${question} \n Magic 8 Ball:${answer[4]}`);
 } else if(question==6){
-  console.log(`Question:${question} /n Magic 8 Ball:${answer[5]}`);
+  console.log(`Question:${question} \n Magic 8 Ball:${answer[5]}`);
 } else if (question==7){
   console.log(`Question:${question} \n Magic 8 Ball:${answer[6]}`);
 } else if(question==8){
