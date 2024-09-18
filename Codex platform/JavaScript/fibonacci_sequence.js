079b0ce91af7fd9ff1b12c289005eb6402d7b151
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-18 17:48:44 +0545

    Create fibonacci_sequence.js

---
+++ Codex platform/JavaScript/fibonacci_sequence.js
@@ -0,0 +1,21 @@
+// Write code below 💖
+let limit=50;
+let a=0;
+let b=1;
+while(a<=limit){
+  console.log(a);
+  let temp=a+b;
+  a=b;
+  b=temp;
+}
+
+/* Write code below 💖
+let limit=50;
+let a=0;
+let b=1;
+while(a<=limit){
+  console.log(a);
+  let temp=a+b;
+  a=b;
+  b=temp;
+} */
