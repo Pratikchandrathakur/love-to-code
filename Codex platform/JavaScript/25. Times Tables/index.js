366c9b303a413ce2345aaa9941bf55251db244df
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-18 16:10:05 +0545

    Create index.js

---
+++ Codex platform/JavaScript/25. Times Tables/index.js
@@ -0,0 +1,7 @@
+// Write code below 💖
+let numbers=[0,1,2,3,4,5,6,7,8,9,10]
+let multiple=9;
+
+for(let i=0; i<numbers.length;i++){
+  console.log(`${multiple} x ${numbers[i]} = ${multiple * numbers[i]}`);
+}
