cded739c613868be4131ddda55969a15f605620b
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-18 17:49:52 +0545

    Update fibonacci_sequence.js

--- Codex platform/JavaScript/fibonacci_sequence.js
+++ Codex platform/JavaScript/fibonacci_sequence.js
@@ -9,13 +9,6 @@ while(a<=limit){
   b=temp;
 }
 
-/* Write code below 💖
-let limit=50;
-let a=0;
-let b=1;
-while(a<=limit){
-  console.log(a);
-  let temp=a+b;
-  a=b;
-  b=temp;
+/* Output:
+0,1,1,2,3,5,8,13,21,34
 } */
