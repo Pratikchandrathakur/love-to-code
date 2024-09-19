d3167c39ea7a8d049b463284a4178396decd8c5a
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-19 18:45:20 +0545

    Create script.js

---
+++ Codex platform/JavaScript/27 Where's Waldo/script.js
@@ -0,0 +1,19 @@
+// Write code below 💖
+
+const characters = [
+  "The Wally Watchers",
+  "Wilma",
+  "Fritz",
+  "Wizard Whitebeard",
+  "Odlaw",
+  "Waldo",
+  "Woof"
+];
+if (characters.includes("Waldo")){
+
+waldoIndex=characters.indexOf("Waldo");
+console.log(`Found Waldo at index ${waldoIndex}!`);
+}else{
+  console.log("Not found");
+}
+
