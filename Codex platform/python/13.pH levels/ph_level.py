afa464fc5c966d928e7e05dd5948662089d9b087
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-20 17:30:48 +0545

    Create ph_level.py

---
+++ Codex platform/python/13.pH levels/ph_level.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+ph=float(input("Enter the value from 0 to 14 ")) ##input the value from 0 to 14
+if(ph>7):
+  print("Basic")
+elif (ph<7):
+  print("Acidic")
+else:
+  print("Neutral")
