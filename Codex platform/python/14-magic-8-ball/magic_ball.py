e2601c7afc172b37b03bf492d1915dd1e4843e1b
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-20 17:48:02 +0545

    Create magic_ball.py

---
+++ Codex platform/python/14-magic-8-ball/magic_ball.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+import random
+answer_num=random.randint(1,8)
+question_num=random.randint(1,2)
+Answer=["es - definitely.","It is decidedly so.","Without a doubt.","Reply hazy, try again.","Ask again later.","Better not tell you now."," My sources say no.","Outlook not so good.","Very doubtful."]
+Question=[" Is Codédex better than Udemy yet?","Are you loving this platform or not?"]
+print(Question[question_num])
+print(Answer[answer_num])
+
