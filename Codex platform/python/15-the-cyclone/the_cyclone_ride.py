061855a8285a4d7a76de4ebe7f088ba866be72a2
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-20 17:58:04 +0545

    Create the_cyclone_ride.py

---
+++ Codex platform/python/15-the-cyclone/the_cyclone_ride.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+height_in_cm=100
+credits=8
+if(height_in_cm>137 and credits>10):
+  print("Enjoy the ride!")
+elif(height_in_cm<=137 and credits>10):
+  print("You are not tall enough to ride.")
+elif(height_in_cm>137 and credits<10):
+  print("You don't have enough credits.")
+else:
+  print("You have not met either requirement")
+
