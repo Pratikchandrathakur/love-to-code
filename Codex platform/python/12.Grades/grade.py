eb549d42938ba2b82dbabcbae1fa8b9b5af9f36d
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-20 12:13:34 +0545

    Create grade.py

---
+++ Codex platform/python/12.Grades/grade.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+grade=int(input("Enter Your Grades "))
+
+if (grade>=55 and grade<=100):
+  print("You Passed")
+
+elif (grade>100):
+  print("Invalid Grade")
+else:
+  print("You failed")
