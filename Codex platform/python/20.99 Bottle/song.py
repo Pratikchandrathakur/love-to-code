b3251fe0af6f117ef353d816c25bbbee41f07cc2
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-24 13:46:17 +0545

    Create song.py

---
+++ Codex platform/python/20.99 Bottle/song.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+song_name="99 bottles of beer "
+for i in range(4):
+  if(i==1):
+   print(f"{song_name}")
+  elif(i==2):
+    print("Take one down, pass it around")
+  else:
+    print(f"{song_name} on the wall")
+
+
