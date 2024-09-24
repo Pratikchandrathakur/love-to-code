5c517f06fa55ed7b54f1fca9af1633d358571ed1
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-24 13:11:52 +0545

    Create detention.py

---
+++ Codex platform/python/19.Detention/detention.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+
+count=1;
+for count in range(100):
+  print("I will not use Snapchat in class")
+
+print("finished 100 as in range it start count from 0 to (number-1) which is specified in range")
