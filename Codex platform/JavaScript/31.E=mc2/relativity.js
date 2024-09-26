f3a0e4a32a3b816c895e420866ed6908e4f2a31e
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-26 16:17:40 +0545

    Create relativity.js

---
+++ Codex platform/JavaScript/31.E=mc2/relativity.js
@@ -0,0 +1,22 @@
+// Write code below 💖
+
+// function greetings(name){
+//    console.log("Hi! My name is " + name +".");
+// }
+
+// greetings("Pratik");
+
+// function greetings(name, age) {
+//   console.log("Hi! My name is " + name + ".");
+//   console.log("I am " + age + " years old.");
+// }
+
+// greetings("Pratik", 24);
+
+function relativityTheory(mass){
+  const c =299792458;
+  let E=mass*c*c;
+  return E
+}
+
+console.log(relativityTheory(50))
