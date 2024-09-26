fa4cd4b12ac2739b2330725f9763c9cd27c46772
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-26 17:01:49 +0545

    Create clout.js

---
+++ Codex platform/JavaScript/32.Clout/clout.js
@@ -0,0 +1,34 @@
+// Write code below 💖
+
+const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
+const recentInstagramViews = [936, 2576, 453, 7013, 5489, 7402, 3921];
+const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
+
+function mean(viewsArray) {
+    let totalViews = 0;
+    for (let i = 0; i < viewsArray.length; i++) {
+        totalViews += viewsArray[i];
+    }
+    return totalViews / viewsArray.length;
+}
+function median(viewsArray){
+   const sortedStats = viewsArray.sort((a, b) => a - b);
+   const middleIndex = Math.floor(sortedStats.length / 2);
+   // Step 3: Determine the median
+    if (sortedStats.length % 2 === 1) {
+        return sortedStats[middleIndex];
+    } else {
+        const middleValuesSum = sortedStats[middleIndex] + sortedStats[middleIndex - 1];
+        return middleValuesSum / 2;
+    }
+} 
+
+console.log("TikTok \n Mean: " + mean(recentTikTokViews)+ "\n Median:"+ median(recentTikTokViews));
+console.log("\n");
+console.log("Instagram \n Mean: " + mean(recentInstagramViews)+ "\n Median:"+ median(recentInstagramViews));
+console.log("\n");
+console.log("YouTube \n Mean: " + mean(recentYouTubeViews)+ "\n Median:"+ median(recentYouTubeViews));
+
+
+
+
