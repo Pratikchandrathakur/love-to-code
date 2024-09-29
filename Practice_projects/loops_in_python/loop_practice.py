804baeb5fede47ae92a7bbf34de7f9581b4aed07
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-09-29 09:22:14 +0545

    Create loop_practice.py

---
+++ Practice_projects/loops_in_python/loop_practice.py
@@ -0,0 +1,24 @@
+# For loops - start to finish of an iterate
+vegetables = ["cucumber", "spinach", "cabbage"]
+for veggies in vegetables:
+    print(veggies)
+
+for i in range(5):
+    print(i)
+
+word = "Python"
+for letter in word:
+    print(letter)
+
+# While loops - execute as long as True
+i = 1
+
+while i < 10:
+    print(i)
+    i += 1
+
+# Simple access program:
+password=""
+while password !="Superhuman123":
+  password=input("Enter the secret password")
+print("Access Granted!")
