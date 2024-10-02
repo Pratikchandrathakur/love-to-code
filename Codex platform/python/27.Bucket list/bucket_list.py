89c63c454c4d3eed6bf4bd185a77008d79f7eb68
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 14:35:14 +0545

    Create bucket_list.py

---
+++ Codex platform/python/27.Bucket list/bucket_list.py
@@ -0,0 +1,21 @@
+# Write code below 💖
+
+things_to_do = [
+  '🚀 Create the dopest learn to code platform ever.',
+  '⛰️ Hike the Pacific Crest Trail.',
+  '🏡 Build an A-frame house and raise some goats.',
+  '🌏 Live somewhere in Asia for a year.',
+  '🎸 Release an album.',
+  '📝 Write a book.',
+  '🏆 Reach 100k subscribers on YouTube.',
+  '🚐 Road trip with the fam.',
+  '🍳 Open a cozy diner upstate.',
+  '👴🏻 Grow old with no regrets.'
+]
+
+for thing in range(len(things_to_do)):
+  print(things_to_do[thing])
+
+
+# for thing in things_to_do: --->2nd way
+#   print(thing)
