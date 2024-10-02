8d4444a33ff772118d27b5c541f74c50a8a00fc4
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 15:33:50 +0545

    Create calculator.py

---
+++ Codex platform/python/31.Calculator/calculator.py
@@ -0,0 +1,20 @@
+# Write code below 💖
+
+def add(a,b):
+  return a+b
+def substract(a,b):
+  return a-b
+def multiply(a,b):
+  return a*b
+def divide(a,b):
+  return a/b
+def exp(a,b):
+  return a**b
+
+print(f"the sum of two numbers is:{add(5,6)}")
+print(f"the substraction of two numbers is: {substract(5,6)}")
+print(f"the multiplication of two number is: {multiply(5,6)}")
+print(f"the division of two number is:{divide(5,6)}")
+print(f"the exponent of two number is:{exp(5,6)}")
+
+
