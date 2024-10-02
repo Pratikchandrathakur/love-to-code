0ee60a845623a92fc714e03a6ff9f558207fdaa3
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 14:45:14 +0545

    Update dry.py

--- Codex platform/python/28.D.R.Y/dry.py
+++ Codex platform/python/28.D.R.Y/dry.py
@@ -1,2 +1,9 @@
-map(function, iterable, *iterables)
-Return an iterator that applies function to every item of iterable, yielding the results. If additional iterables arguments are passed, function must take that many arguments and is applied to the items from all iterables in parallel. With multiple iterables, the iterator stops when the shortest iterable is exhausted.
+# Write code below 💖
+#len function -->Return the length (the number of items) of an object. The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
+
+#range function-->Rather than being a function, range is actually an immutable sequence type, as documented in Ranges and Sequence Types — list, tuple, range.
+#max function -->Return the largest item in an iterable or the largest of two or more arguments.
+#min function --> Return the smallest item in an iterable or the smallest of two or more arguments.
+
+# map(function, iterable, *iterables)
+# Return an iterator that applies function to every item of iterable, yielding the results. If additional iterables arguments are passed, function must take that many arguments and is applied to the items from all iterables in parallel. With multiple iterables, the iterator stops when the shortest iterable is exhausted.
