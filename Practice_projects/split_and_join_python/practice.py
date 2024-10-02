1e3ac1e7798832c4059a07017df486a209789323
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 19:22:09 +0545

    Update practice.py

--- Practice_projects/split_and_join_python/practice.py
+++ Practice_projects/split_and_join_python/practice.py
@@ -1,5 +1,6 @@
 sentence="this is a best python course"
 sentence_split=sentence.split() #It split the sting into list using delimiter- by default it is space here.
 print(sentence_split)
-sentence_join=" ".join(sentence_split) #It join the list into string using delimiter - " ".
+sentence_join=" ".join(sentence_split) #It join the list into string using delimiter -> " ".
+sentence_join="-".join(sentence_split) #It join the list into string and add it by "-" using delimiter -> "-".
 print(sentence_join)
