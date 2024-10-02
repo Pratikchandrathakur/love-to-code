d770729a902ffd0fe675d73a211ce8a707e27373
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 19:20:49 +0545

    Create practice.py

---
+++ Practice_projects/split_and_join_python/practice.py
@@ -0,0 +1,5 @@
+sentence="this is a best python course"
+sentence_split=sentence.split() #It split the sting into list using delimiter- by default it is space here.
+print(sentence_split)
+sentence_join=" ".join(sentence_split) #It join the list into string using delimiter - " ".
+print(sentence_join)
