feddc6c60cfed09bdf996371674a7a29dbcaec10
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 20:54:53 +0545

    Create import.py

---
+++ Practice_projects/importing_python/import.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+import datetime #just using import
+now=datetime.datetime.now()
+print
+
+from datetime import datetime #importing datetime only from datetime module
+now=datetime.now()
+print(now)
+
+from datetime import datetime as dt #using alias to import datetime to represent as dt to make ease of code.
+now=dt.now()
+print(now)
