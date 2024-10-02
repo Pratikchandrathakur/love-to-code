9e5bb12a00fd733f4f2d4f8ea9ca21e841202105
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 14:20:00 +0545

    Create reading_list.py

---
+++ Codex platform/python/25.Reading List/reading_list.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+books=['Zero to One',
+'The Lean Startup',
+'The Mom Test',
+'Make It Stick',
+'Life in Code']
+
+books.append('Zero to Sold') #add Zero to Sold book at last
+print(books)
+books.remove('Zero to One') #remove this element from the book
+books.remove('The Lean Startup')  #remove this element from the book
+
+print(books)
