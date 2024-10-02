32a16406dff0c08c33464c0fe7f62c8e015febf8
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 18:33:17 +0545

    Create order.py

---
+++ Codex platform/python/33.Drive-thru/order.py
@@ -0,0 +1,24 @@
+# Write code below 💖
+person=input("Enter Your Name Please! ")
+def welcome(name):
+  welcome_menu=['🍔 Cheeseburger','🍟 Fries','🥤 Soda','🍦 Ice Cream','🍪 Cookie']
+  print("Hello "+name)
+  choose=int(input("Enter 1 for 🍔 Cheeseburger , 2 for 🍟 Fries,\n 3 for 🥤 Soda, 4 for🍦 Ice Cream, 5 for🍪 Cookie "))
+  if choose==1:
+    return "🍔 Cheeseburger"
+  elif choose==2:
+    return "🍟 Fries"
+  elif choose==3:
+    return "🥤 Soda"
+  elif choose==4:
+    return "🍦 Ice Cream"
+  elif choose==5:
+    return "🍪 Cookie "
+  else:
+    return "Please enter valid input"
+
+ordering=welcome(person)
+def get_item(order):
+  print(f"Here is your ordered {order}")
+
+get_item(ordering)
