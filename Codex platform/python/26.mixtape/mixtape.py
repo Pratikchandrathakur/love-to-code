c5ab2fb4da8a6447f323a762c29f1b0ed379cf9b
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 14:25:22 +0545

    Create mixtape.py

---
+++ Codex platform/python/26.mixtape/mixtape.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+# 💿 Theme: Indie Travel Songs
+
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+
+for song in playlist:
+  print(f"Playing song named: {song}📻 ")
