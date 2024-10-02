16adefc336e9d713b7d21dc10339c7f9afa7a867
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-02 14:08:00 +0545

    Create inventory.py

---
+++ Codex platform/python/24.Inventory/inventory.py
@@ -0,0 +1,5 @@
+# Write code below 💖
+
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+print(f"the part no {min(lego_parts)} is minimum in stock for shipping")
+print(f"the part no {max(lego_parts)} is maximum in stock for shipping")
