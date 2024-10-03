04783a89691a222f52d657cb68a98eb108ebfd78
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-03 18:45:53 +0545

    Create objectPractise.js

---
+++ Practice_projects/Object_methods_Js/objectPractise.js
@@ -0,0 +1,27 @@
+// Write code below 💖
+
+const phone = {
+  model: "iPhone 11 Pro Max",
+  brand: "Apple",
+  version: 11.6,
+  owner: "John Appleseed",
+  ringtone: function () {
+    console.log("Ring, ring! Ring, ring! 📲")
+  }
+}
+
+phone.ringtone();
+// Output: Ring, ring! Ring, ring! 📲
+
+const phone1 = {
+  model: "Android",
+  brand: "Samsung",
+  version: 16.1,
+  owner: "Jane Doe",
+  ringtone () {
+    console.log("Ring, ring! Ring, ring! 📲")
+  }
+}
+
+phone1.ringtone();
+// Output: Ring, ring! Ring, ring! 📲
