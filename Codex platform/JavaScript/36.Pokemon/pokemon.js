b5952ab71c0bd89cb44815477783bb31fbc9c2a2
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-03 18:41:00 +0545

    Create pokemon.js

---
+++ Codex platform/JavaScript/36.Pokemon/pokemon.js
@@ -0,0 +1,14 @@
+// Write code below 💖
+
+pokemon = {
+  name: "Pikachu",
+  type: "Electric ⚡️",
+  level: 25,
+}
+// 1st task
+pokemon.isCaught=false;
+console.log(pokemon)
+// 2nd task
+pokemon.isCaught=true
+pokemon.name="firebird"
+console.log(pokemon)
