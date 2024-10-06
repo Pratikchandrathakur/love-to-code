f536bacbde44dc52a6aa0379359c1672217a1d5f
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-06 12:32:01 +0545

    Create class2.js

---
+++ Practice_projects/Class in JavaScript/class2.js
@@ -0,0 +1,21 @@
+// Write code below 💖
+class Product{
+  constructor(product,price){  //-- Constructor inside class can be used to define normal properties of each objects instances of this class.
+    this.Product=product;
+    this.Price=price;
+  }
+  displayProduct(){  // -- these methods are specially defined to do certain task with the properties defined in constructor.
+    console.log(`Product : ${this.Product} \nPrice: $${this.Price}`)
+  }
+  calculateSaleTax(saleTax=0.08){
+    return this.Price+(this.Price)*saleTax  
+  }
+}
+const product1=new Product("Shirt",18.99);
+const product2=new Product("Pant",23.99);
+const product3=new Product("Underwear",60);
+
+// Checking for product 3:
+product3.displayProduct();
+total=product3.calculateSaleTax()
+console.log(`The (total price with tax): $${total}`)
