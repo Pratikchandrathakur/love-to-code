6e086e79ff6e98441b4d134126dbc6d866f9e005
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-14 09:21:53 +0545

    Create letter_to_self.py

---
+++ Codex platform/python/43.Zen of python/letter_to_self.py
@@ -0,0 +1,6 @@
+# Write code below 💖
+print("2024 Sept 3");
+print("I am feeling good but little low as i miss my scholarship today.");
+print("I want to become a developer who can make products \n using different programming languages.");
+print("Save money for yourself and make a fortune if you have fortune \n then congrats live for your loved ones for social good love your wife and children but don't ever spoil them");
+print("2024 Sept 3");
