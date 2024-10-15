66451e7bc4c078e7205e7495ff5d20627d9e8606
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-15 09:44:32 +0545

    Create script.js

---
+++ Codex platform/JavaScript/41.hotline/script.js
@@ -0,0 +1,5 @@
+// Write code below 💖
+let drakePicTwo=document.getElementById('drake-pic-2')
+let heading=document.getElementById('heading-2')
+drakePicTwo.setAttribute("src","https://i.imgur.com/RGbh6zY.png");
+heading.innerText="Learn By Doing";
