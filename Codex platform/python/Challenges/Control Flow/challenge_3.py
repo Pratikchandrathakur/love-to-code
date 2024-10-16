d942bf52ca651b33991822efd9c92103740aaced
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-16 18:53:05 +0545

    Create challenge_3.py

---
+++ Codex platform/python/Challenges/Control Flow/challenge_3.py
@@ -0,0 +1,22 @@
+#Snapple facts.
+# Write code below 💖
+
+import random
+
+random_num=random.randint(0,5)
+
+if random_num==0:
+  print('Flamingos turn pink from eating shrimp.')
+elif random_num==1:
+  print('The only food that doesn\'t spoil is honey.')
+elif random_num==2:
+  print('Shrimp can only swim backwards.')
+elif random_num==3:
+  print('A taste bud\'s life span is about 10 days.')
+elif random_num==4:
+  print('It is impossible to sneeze while sleeping.')
+elif random_num==5:
+  print('It is illegal to sing off-key in North Carolina.')
+else:
+  print("So soory out or range")
+
