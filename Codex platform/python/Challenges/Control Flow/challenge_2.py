e5dd1208eb9923261d85323277fa9d2ae5276817
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-16 18:46:23 +0545

    Create challenge_2.py

---
+++ Codex platform/python/Challenges/Control Flow/challenge_2.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+grade=int(input("Enter your Grade in integer"))
+if grade==9:
+  print('Freshman')
+elif grade==10:
+  print('Sophomore')
+elif grade==11:
+  print('Junior')
+elif grade==12:
+  print('Senior')
+else:
+  print('TBD')
