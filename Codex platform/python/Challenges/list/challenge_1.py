dee3eea5daa56efcbe5522acfdc66a48bc7da259
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-16 16:51:01 +0545

    Create challenge_1.py

---
+++ Codex platform/python/Challenges/list/challenge_1.py
@@ -0,0 +1,6 @@
+# Write code below 💖
+wishlist=['I want to earn atleast 50000 as my starting salary', 'I want have a beautiful house','I want to have a good and strong family car which should be longlasting'];
+print(wishlist)
+# wishlist.append('I want to gift a superbike to my brother');
+
+
