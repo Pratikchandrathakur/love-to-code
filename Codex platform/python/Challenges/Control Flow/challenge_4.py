e275bad47d9497d37fc0849ab4522deb4079e209
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-16 19:04:00 +0545

    Create challenge_4.py

---
+++ Codex platform/python/Challenges/Control Flow/challenge_4.py
@@ -0,0 +1,15 @@
+# Write code below 💖
+# Season of the year.
+
+month=int(input("Enter the months number you want to check season for "))
+
+if month==1 or month==2 or month==3:
+  print('Winter 🌨️')
+elif month==4 or month==5 or month==6:
+  print( 'Spring 🌱')
+elif month==7 or month== 8 or month== 9:
+  print('Summer 🌻')
+elif month==10 or month==11 or month==12:
+  print('Autumn 🍂')
+else:
+  print("Invalid")
