36c11808f1d989078020fe45d5a41a5cec032697
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-16 18:41:36 +0545

    Create challenge_1.py

---
+++ Codex platform/python/Challenges/Control Flow/challenge_1.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+rating=4.8
+
+if rating > 4.5:
+  print('Extraordinary')
+elif rating > 4:
+  print('Excellent')
+elif rating > 3:
+  print('Good')
+elif rating > 2:
+  print('Fair')
+else:
+  print('Poor')
