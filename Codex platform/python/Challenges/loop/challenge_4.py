d593425730fd86cce321492e27d4b0f7f1f76982
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 07:27:16 +0545

    Create challenge_4.py

---
+++ Codex platform/python/Challenges/loop/challenge_4.py
@@ -0,0 +1,23 @@
+# #✳️ Asterisks
+# Medium
+
+# 88
+
+# 10
+# Use a for loop and range() function to display the following pattern in the terminal using a bunch of asterisks *:
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# # ... and so on
+
+# It should look like this but with 24 rows total. The first row should have one asterisk and the last row should have 24 asterisks.
+
+# Note: Make sure an empty space is between each asterisk.
+
+# Write code below 💖
+
+for i in range(1,25):
+  print('* ' * i)
