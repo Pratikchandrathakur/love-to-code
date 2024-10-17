f93fcd1b563e00e4b284ddb299d6d6acde4f2f26
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 09:05:55 +0545

    Update challenge_1.py

--- Codex platform/python/Challenges/loop/challenge_1.py
+++ Codex platform/python/Challenges/loop/challenge_1.py
@@ -1,3 +1,17 @@
+# 🚙 Are We There Yet?-->Easy
+
+# “Are we there yet?” is a phrase that has existed for as long as there are children, vehicles, and road trips.
+
+# First, ask the user “Are we there yet?” using the input() function and store it in an answer variable.
+
+# Then, use a while loop that asks the user “Are we there yet?” over and over again until they respond with a “Yes”.
+
+# The output could look something like this:
+
+# Are we there yet? One more hour
+# Are we there yet? Almost there
+# Are we there yet? Don't make me pull over
+# Are we there yet? Yes
 # Write code below 💖
 
 answer=input('Are we there yet? ')
