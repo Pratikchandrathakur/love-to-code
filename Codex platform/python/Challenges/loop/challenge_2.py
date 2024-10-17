dcb2c70ee6b68d2e521bb8000c8529d930177946
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 09:06:59 +0545

    Update challenge_2.py

--- Codex platform/python/Challenges/loop/challenge_2.py
+++ Codex platform/python/Challenges/loop/challenge_2.py
@@ -1,3 +1,25 @@
+# 🥳 New Year Countdown-->Medium
+# Ring in the New Year! A New Year's Eve party doesn't feel complete without a countdown from 10 to 1.
+
+# Use a for loop and the range() function for a countdown.
+
+# The output should look like:
+
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+# Happy New Year! 🥳
+
+# Counting down from 10 to 1 on each line and then a Happy New Year message at the end.
+
+# Note: Feel free to end your message with any emoji you want!
 # Write code below 💖
 for count in range(10,0,-1):
   print(count)
