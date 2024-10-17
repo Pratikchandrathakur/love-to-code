c081e6f739a8090e61fae0340dd40865bfb5f9ea
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 14:02:45 +0545

    Update challenge_2.py

--- Codex platform/python/Challenges/list/challenge_2.py
+++ Codex platform/python/Challenges/list/challenge_2.py
@@ -1,20 +1,38 @@
-# Write code below 💖
+# 🎟️ Lottery-->Medium
+# Are you feeling lucky? Let's replicate a small lottery that decides a winner based on random numbers. This challenge will use the random module.
 
+# Create two empty lists called my_numbers and winning_numbers.
+
+# Loop over a range of 5 steps (i.e., range(0,5)) and populate both lists with a random number between 1 and 69.
+
+# Outside the loop, add one more number to each list that is between 1 and 26.
+
+# Lastly, print the my_numbers and winning_numbers lists to the terminal. The output could look like this:
+
+# My Numbers: [59, 66, 28, 52, 10, 12]
+# Winning Numbers: [26, 27, 37, 35, 47, 8]
+
+
+# Write code below 💖
 import random
+
 my_numbers=[]
 winning_numbers=[]
-def random_number_populator(list_name):
-#  Generates a sequence of 5 random numbers between 1 to 69 
-  for num in range(0,5):
-    num=random.randint(1,69)
-    list_name.append(num)
 
+for num in range(0,5):
+  my_numbers.append(random.randint(1,69))
+for num in range(0,5):
+  winning_numbers.append(random.randint(1,69))
 
-random_number_populator(my_numbers)
 my_numbers.append(random.randint(1,26))
-random_number_populator(winning_numbers)
 winning_numbers.append(random.randint(1,26))
 
-print(f"My Numbers:{my_numbers}")
-print(f"Winning Numbers:{winning_numbers}")
+print(f"My Numbers: {my_numbers}")
+print(f"Winning Numbers: {winning_numbers}")
+
+
+
+
+
+
 
