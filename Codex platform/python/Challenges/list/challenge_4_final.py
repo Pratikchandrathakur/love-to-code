d1219a0a26b525e191befecf75afc20a3bf4034d
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 21:31:48 +0545

    Create challenge_4_final.py

---
+++ Codex platform/python/Challenges/list/challenge_4_final.py
@@ -0,0 +1,21 @@
+# 🧾 Split the Bill-->Medium
+# Suppose you and three of your friends all went out for a meal together. The bill comes due, and all of you decide to split the bill evenly.
+
+# Here is a Python list of prices for what was ordered:
+
+# bill = [13.99, 28.75, 9.99, 9.99, 6.95, 7.45, 16.45, 16.45]
+
+# Using this bill list, calculate the total amount and then divide it four ways.
+
+# Initialize the total with zero and loop through the bill list to add everything up.
+
+# With the total, store what each person has to pay in a my_share variable and then print the result to the terminal.
+
+# Write code below 💖
+bill = [13.99, 28.75, 9.99, 9.99, 6.95, 7.45, 16.45, 16.45]
+total=0
+for amount in bill:
+  total+=amount
+print(f"The total amount : {round(total)}")
+my_share=total/4
+print(f"Each person share will be : {my_share}")
