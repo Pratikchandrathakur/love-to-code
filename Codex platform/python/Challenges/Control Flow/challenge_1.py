bfb5a87e43a1a89522772b64e2f6ef280d194c10
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 07:48:06 +0545

    Update challenge_1.py

--- Codex platform/python/Challenges/Control Flow/challenge_1.py
+++ Codex platform/python/Challenges/Control Flow/challenge_1.py
@@ -1,3 +1,18 @@
+# ⭐️ Food Ratings -->Easy
+# In a five-star restaurant review system (⭐️⭐️⭐️⭐️⭐️), the stars typically represent the different levels of satisfaction.
+
+# But what does each of the stars mean?
+
+# Start by creating a rating variable and set it equal to a decimal number.
+
+# Make a rating system using an if/elif/else statement:
+
+# rating greater than 4.5, print 'Extraordinary'
+# rating greater than 4, print 'Excellent'
+# rating greater than 3, print 'Good'
+# rating greater than 2, print 'Fair'
+# Everything else, print 'Poor'
+
 # Write code below 💖
 
 rating=4.8
