8a86b07cdf879f11927dcbbbef8fe589fb0c9b76
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 23:32:55 +0545

    Update challenge1.js

--- Codex platform/JavaScript/Challenges/conditionals/challenge1.js
+++ Codex platform/JavaScript/Challenges/conditionals/challenge1.js
@@ -1,1 +1,12 @@
-//hello there
+// 🦉 Nite Owl-->Easy
+// People tend to be either an early bird or a night owl. There are several factors that go into this, such as genetics, age, lifestyle, and more.
+
+// Let's use conditionals to see which one you are!
+
+// Start by creating a wakeUp variable and set it equal to the time you wake up, between 0 and 23.
+
+// Test for early bird vs. night owl with an if/else statement:
+
+// wakeUp is < 8, log "Early bird gets the worm! 🌅"
+// Otherwise, log "But who wants the worm? 🌃"
+
