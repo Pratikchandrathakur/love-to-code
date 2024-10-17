c1c50512cd9b08eef4bbaf2d04d8b835ec4a86ee
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 16:37:46 +0545

    Create challenge_3.py

---
+++ Codex platform/python/Challenges/list/challenge_3.py
@@ -0,0 +1,24 @@
+# 🏆 The Oscars-->Medium
+# Every year at the Oscars, the Academy Award for Best Picture is awarded to a single film. It's usually the last award presented and is considered to be the most prestigious.
+
+# Let's use a Python list to document the recent winners!
+
+# Start with the following list:
+
+# best_pictures = [
+#   '2019 - Parasite',
+#   '2018 - Green Book',
+#   '2017 - The Shape of Water',
+#   '2016 - Moonlight',
+#   '2015 - Spotlight',
+#   '2014 - Birdman',
+#   '2013 - 12 Years a Slave',
+#   '2012 - Argo',
+#   '2011 - The Artist',
+#   '2010 - The King\'s Speech'
+# ]
+
+# Google the Best Picture winners from 2020, 2021, and 2022. Then, add them to the front of the best_pictures list.
+
+# Lastly, print the best_pictures list. Make sure that the updated list starts with the winner for 2022, then 2021, then 2020, and so on.
+
