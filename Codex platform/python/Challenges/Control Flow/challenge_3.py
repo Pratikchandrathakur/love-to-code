2328c1e6a81f339acd22d35d62b494ad503ed11e
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 07:50:04 +0545

    Update challenge_3.py

--- Codex platform/python/Challenges/Control Flow/challenge_3.py
+++ Codex platform/python/Challenges/Control Flow/challenge_3.py
@@ -1,4 +1,17 @@
-#Snapple facts.
+# 🤯 Snapple Facts-->Medium
+# Snapple is a famous tea drink brand from Queens, New York. Each bottle cap comes with a silly fun fact.
+
+# Use the random module to create a number from 0 to 5.
+
+# Then use an if/elif/else statement to print out one of these six random Snapple facts:
+
+# 0 - 'Flamingos turn pink from eating shrimp.'
+# 1 - 'The only food that doesn\'t spoil is honey.'
+# 2 - 'Shrimp can only swim backwards.'
+# 3 - 'A taste bud\'s life span is about 10 days.'
+# 4 - 'It is impossible to sneeze while sleeping.'
+# 5 - 'It is illegal to sing off-key in North Carolina.'
+
 # Write code below 💖
 
 import random
