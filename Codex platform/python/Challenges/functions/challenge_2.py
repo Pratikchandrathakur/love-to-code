68a7f9732ea82a1a8a46974c83d9624dd3cb49ca
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 07:46:55 +0545

    Create challenge_2.py

---
+++ Codex platform/python/Challenges/functions/challenge_2.py
@@ -0,0 +1,22 @@
+# 🧮 Average Number -->Easy
+# Finding an average can be a useful way to understand the underlying trends of some data.
+
+# Write an average() function that takes in two parameters:
+
+# num1
+# num2
+# It should calculate and return the average of the two numbers.
+
+# The average of num1 and num2 is:
+
+# (num1+num2)/2
+
+# Note: Do not use the math module.
+
+# Write code below 💖
+
+def average(num1,num2):
+  average=(num1+num2)/2
+  return average
+
+print(average(5,4))
