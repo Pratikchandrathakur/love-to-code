dd0f09b3f54dcf331b19a7e35827f621e8fa55de
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 16:46:46 +0545

    Update challenge_3.py

--- Codex platform/python/Challenges/list/challenge_3.py
+++ Codex platform/python/Challenges/list/challenge_3.py
@@ -22,3 +22,23 @@
 
 # Lastly, print the best_pictures list. Make sure that the updated list starts with the winner for 2022, then 2021, then 2020, and so on.
 
+# Write code below 💖
+best_pictures = [
+  '2019 - Parasite',
+  '2018 - Green Book',
+  '2017 - The Shape of Water',
+  '2016 - Moonlight',
+  '2015 - Spotlight',
+  '2014 - Birdman',
+  '2013 - 12 Years a Slave',
+  '2012 - Argo',
+  '2011 - The Artist',
+  '2010 - The King\'s Speech'
+]
+best_pictures.insert(0,'2020 - Nomadland')
+best_pictures.insert(0,'2021 - CODA')
+best_pictures.insert(0,'2022 - Everything Everywhere All at Once')
+print(best_pictures)
+
+
+
