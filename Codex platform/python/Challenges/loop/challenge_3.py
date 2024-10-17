1460bc7fd4c8d7b6b220a642e22a05e42171de9c
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 09:07:56 +0545

    Update challenge_3.py

--- Codex platform/python/Challenges/loop/challenge_3.py
+++ Codex platform/python/Challenges/loop/challenge_3.py
@@ -1,3 +1,21 @@
+# 🎲 Dice Roll-->Medium
+# Dice Roll
+# Suppose we have two dice. 🎲 🎲
+
+# Create a game where one guesses the total value after rolling a pair of dice once.
+
+# Each die has the integer values 1 to 6. Since there's two dice, the range of possible values is between 2 and 12 (inclusive).
+
+# Use the random module to “roll” the dice and store the random values of each die variable in a new total variable.
+
+# Until the correct value is guessed, use a while loop to keep prompting the user for a number.
+
+# Use while loops along with methods from the random module to solve this challenge.
+
+# What is the total (2-12)? 10
+# What is the total (2-12)? 7
+# You got it!
+
 # Write code below 💖
 import random
 dice1=random.randint(1,6)
