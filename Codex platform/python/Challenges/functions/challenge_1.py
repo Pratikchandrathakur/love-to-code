2032620103b38956aabe1e0ff0229231795d7a67
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 07:43:30 +0545

    Create challenge_1.py

---
+++ Codex platform/python/Challenges/functions/challenge_1.py
@@ -0,0 +1,25 @@
+# 🍸 James Bond --> Easy
+# James Bond is a classic movie character that fights bad guys and introduces himself in an iconic way: “Bond, James Bond.”
+
+# Write a greetings() function that takes in two parameters:
+
+# first_name
+# last_name
+# Print out the phrase with the last name, followed by a comma, and then the full name.
+
+# Next, call the function using your own name, like:
+
+# greetings('Richard', 'Hendricks')
+
+# The output should look like:
+
+# Hendricks, Richard Hendricks
+
+# Note: This function doesn't have a return statement.
+
+# Write code below 💖
+
+def greetings(first_name,last_name):
+  print(last_name+', '+first_name+' '+last_name)
+
+greetings('Pratik','Chandra Thakur')
