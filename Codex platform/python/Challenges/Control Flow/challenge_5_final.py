c6fd35358eed481b7c256088f197fd17625b32fd
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-17 07:53:03 +0545

    Update challenge_5_final.py

--- Codex platform/python/Challenges/Control Flow/challenge_5_final.py
+++ Codex platform/python/Challenges/Control Flow/challenge_5_final.py
@@ -1,3 +1,26 @@
+# 🧑‍🚀 Planet Weights
+# Hard
+
+# The year is 2199... we have become an interplanetary species and can travel to other planets in the solar system! 🚀
+
+# Create a weight conversion program that:
+
+# Asks the user what their Earth weight is (as a float).
+# Asks the user for a planet number (as an int).
+# Then, use an if/elif/else statement to calculate the user's weight on the destination planet.
+
+# To calculate the user's weight:
+
+# destination weight=Earth weight × relative gravity
+# Number	Planet	Relative Gravity
+# 1	Mercury	0.38
+# 2	Venus	0.91
+# 3	Mars	0.38
+# 4	Jupiter	2.53
+# 5	Saturn	1.07
+# 6	Uranus	0.89
+# 7	Neptune	1.14
+# If the user enters a planet number outside of 1 - 7, print a message that says 'Invalid planet number'.
 # Write code below 💖
 def weight_on_planet(weight_in_earth, planet_number):
     planets = {
