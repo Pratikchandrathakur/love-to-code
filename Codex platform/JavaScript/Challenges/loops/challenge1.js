806d9ebce5eab23f2f211dff05ce60911c443a1e
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-18 09:39:20 +0545

    Update challenge1.js

--- Codex platform/JavaScript/Challenges/loops/challenge1.js
+++ Codex platform/JavaScript/Challenges/loops/challenge1.js
@@ -1,1 +1,23 @@
-//hello there
+// 🚀 Blast Off-->Easy
+// Since Apollo 11, rocket launches are moments of great excitement, especially the countdown.
+
+// Write a small program that uses a for loop to simulate a countdown from 10 to 0, logging each number to the console. Then, log "To infinity and beyond! 🚀" at the very end.
+
+// The output should look like this:
+
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// To infinity and beyond! 🚀
+// Write code below 💖
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+console.log("To infinity and beyond! 🚀");
