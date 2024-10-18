665b87b9a20fd003120a92d4094eb2a3a739d64d
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-18 07:19:06 +0545

    Create challenge3.js

---
+++ Codex platform/JavaScript/Challenges/conditionals/challenge3.js
@@ -0,0 +1,30 @@
+// 🕺 TGIF-->Medium
+// In many parts of the world, we look forward to Friday to kick off a weekend of leisure. Some express this with "TGIF" (short for "Thank God It's Friday").
+
+// Let's use conditionals to tell if the weekend is upon us!
+
+// First, define a day variable with a number between 1 and 7. So 1 represents Monday, 2 represents Tuesday, and so on.
+
+// Based on the value of a day variable, use conditionals to log one of the following:
+
+// If it's a weekday, log "Not Friday, yet!"
+// If it's Friday, log "TGIF 🕺".
+// If it's a weekend, log "Yay, weekends! 🙌".
+// Otherwise, log "Wait, what day is it?"
+
+// Write code below 💖
+
+let day=5
+
+if(day<5){
+  console.log("Not Friday, yet!")
+}
+else if(day===5){
+  console.log("TGIF 🕺")
+}
+else if(day>5){
+  console.log("Yay, weekends! 🙌")
+}
+else{
+  console.log("Wait, what day is it?")
+}
