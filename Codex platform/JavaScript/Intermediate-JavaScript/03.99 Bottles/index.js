01054c153f9737f986fadd00102278d83f1b2273
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-25 04:28:58 +0545

    Create index.js

---
+++ Codex platform/JavaScript/Intermediate-JavaScript/03.99 Bottles/index.js
@@ -0,0 +1,18 @@
+// Change these to string template literals 💖
+
+// for (let i = 99; i > 0; i--) {
+//   console.log(i + " bottles of pop on the wall");
+//   console.log(i + " bottles of pop");
+//   console.log("take one down, pass it around");
+//   console.log(i - 1 + " bottles of pop on the wall");
+//   console.log();
+// }
+// Change these to string template literals 💖
+
+for (let i = 99; i > 0; i--) {
+  console.log(`${i} bottles of pop on the wall`);
+  console.log(`${i} bottles of pop`);
+  console.log(`take one down, pass it around`);
+  console.log(`${i - 1} bottles of pop on the wall`);
+  console.log();
+}
