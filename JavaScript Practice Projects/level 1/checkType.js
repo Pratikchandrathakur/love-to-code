e51cb434c872646b4deb076f6ca98288b5f1d2e1
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2024-10-29 11:44:35 +0545

    Create checkType.js

---
+++ JavaScript Practice Projects/level 1/checkType.js
@@ -0,0 +1,15 @@
+const isHuman=true
+const greet="Hello Mate"
+const favoriteNum=6
+const testString="3.14"
+
+function checkType(value){
+  return typeof(value)
+}
+
+console.log(checkType(isHuman))
+console.log(checkType(greet))
+console.log(checkType(favoriteNum))
+console.log(checkType(testString))
+
+
