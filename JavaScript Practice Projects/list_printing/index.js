b8927adb9ed51d2030f5d3788e17cd804c63c575
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2025-08-01 20:51:13 +0545

    Update index.js

--- JavaScript Practice Projects/list_printing/index.js
+++ JavaScript Practice Projects/list_printing/index.js
@@ -1,21 +1,20 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-listItem="";
-// Assign it to an empty string to begin with
-for (let i = 0; i < myLeads.length; i++) {
-    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItem+="<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a href='myLeads[i]' target='_blank'> " + myLeads[i] + "</a></li>"
+    }
+    ulEl.innerHTML = listItems  
 }
-// 3. Render the listItems inside the unordered list using ulEl.innerHTML
-
-ulEl.innerHTML=listItem;
-
