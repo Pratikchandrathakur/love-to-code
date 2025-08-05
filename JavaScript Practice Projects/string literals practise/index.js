9f9e88b70e15c48debfd1e0e98ab28ebd524983a
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2025-08-05 18:46:29 +0545

    Create index.js

---
+++ JavaScript Practice Projects/string literals practise/index.js
@@ -0,0 +1,9 @@
+// template strings/literals
+
+const recipient = "James"
+// Create a new variable, sender, and set its value to your name
+const sender = "Per Harald Borgen"
+// Use your sender variable instead of "Per"
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+
+console.log(email)
