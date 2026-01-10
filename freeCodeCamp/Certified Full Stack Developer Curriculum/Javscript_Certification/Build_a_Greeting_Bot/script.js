39c5712232364fbaab3e2fb3071c06b384740892
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-10 19:45:32 +0545

    Add greeting bot script with introduction messages

---
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javscript_Certification/Build_a_Greeting_Bot/script.js
@@ -0,0 +1,14 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+let botLocationSentence="I live in " + botLocation+".";
+console.log(botLocationSentence)
