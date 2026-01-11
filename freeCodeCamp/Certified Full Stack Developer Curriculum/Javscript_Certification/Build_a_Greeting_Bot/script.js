b1bca9b1a60b330d6f1defe035e4d679c4aaaf64
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-11 13:55:15 +0545

    Refactor newNicknameGreeting to use const

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Javscript_Certification/Build_a_Greeting_Bot/script.js
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javscript_Certification/Build_a_Greeting_Bot/script.js
@@ -20,5 +20,10 @@ console.log(nicknameIntroduction);
 
 bot = "awesomeTeacherBot";
 
-let newNicknameGreeting="I love my nickname but I wish people would call me " +bot +"."
-console.log(newNicknameGreeting)
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+let favoriteSubjectSentence="My favorite subject is " + favoriteSubject + "."
+console.log(favoriteSubjectSentence)
