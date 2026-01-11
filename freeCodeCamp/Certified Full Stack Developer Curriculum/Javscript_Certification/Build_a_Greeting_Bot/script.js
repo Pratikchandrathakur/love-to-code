0d9dfe3a617a4e24b4b43cb8e03bab799a19e5b1
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-11 13:52:15 +0545

    Refactor bot introduction and nickname logic

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Javscript_Certification/Build_a_Greeting_Bot/script.js
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javscript_Certification/Build_a_Greeting_Bot/script.js
@@ -10,5 +10,15 @@ console.log("Allow me to introduce myself.");
 const botIntroduction = "My name is " + bot + ".";
 console.log(botIntroduction);
 
-let botLocationSentence="I live in " + botLocation+".";
-console.log(botLocationSentence)
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+let newNicknameGreeting="I love my nickname but I wish people would call me " +bot +"."
+console.log(newNicknameGreeting)
