f9c9ef3eeb2aa29425d1579b3cd9fbf37a3172f5
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-12 19:38:22 +0545

    Add input text and encryption functionality
    Store input text and encrypt it using the translation table.

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
@@ -6,4 +6,7 @@ shifted_alphabet = alphabet[shift:] + alphabet[:shift]
 
 # Create the translation table
 translation_table = str.maketrans(alphabet, shifted_alphabet)
-
+#Input is stored on the text variable
+text = 'hello world'
+#encrpted text will be stored on the encrypted variable using the translate function it chnage the value to mapped table value.
+encrypted_text=text.translate(translation_table)
