43ed9d4e6a954c9f122754b05f6a987eeae7faf5
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-13 14:46:54 +0545

    Change caesar function to return encrypted text
    Refactor caesar function to return encrypted text instead of printing it.

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
@@ -2,8 +2,8 @@ def caesar(text, shift):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet, shifted_alphabet)
-    encrypted_text = text.translate(translation_table)
-    print(encrypted_text)
+    return text.translate(translation_table)
+    
 
 encrypted_text = caesar('freeCodeCamp', 3)
 print(encrypted_text)
