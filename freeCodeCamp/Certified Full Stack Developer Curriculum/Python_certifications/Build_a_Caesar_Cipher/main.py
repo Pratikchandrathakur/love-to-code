9cca9ce63dd2f56d5f616747ecf9cb65a32de060
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-14 22:22:49 +0545

    Fix shift validation in caesar function

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
@@ -1,10 +1,15 @@
 def caesar(text, shift):
-    if True:
-        return 'Shift must be an integer value.'
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
-    return text.translate(translation_table)
+    if isinstance(shift, int):
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        shift = shift % len(alphabet)  # normalize shift
+        shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+        translation_table = str.maketrans(
+            alphabet + alphabet.upper(),
+            shifted_alphabet + shifted_alphabet.upper()
+        )
+        return text.translate(translation_table)
+    else:
+        return "Shift must be an integer value."
 
 
 encrypted_text = caesar('freeCodeCamp', 3)
