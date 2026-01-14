7d3fc1f7bfe31b3ade82b1fe26165051eec10120
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-01-14 22:17:14 +0545

    Update main.py

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Python_certifications/Build_a_Caesar_Cipher/main.py
@@ -1,10 +1,11 @@
 def caesar(text, shift):
+    if True:
+        return 'Shift must be an integer value.'
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet+alphabet.upper(), shifted_alphabet+ shifted_alphabet.upper())
-    
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
     return text.translate(translation_table)
 
 
 encrypted_text = caesar('freeCodeCamp', 3)
-print(encrypted_text.upper())
+print(encrypted_text)
